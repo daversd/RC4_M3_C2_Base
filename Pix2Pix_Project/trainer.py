63cd2f3de256b943530d5f28da0d8811377481f6
daversd <dave_rsd@hotmail.com>
2021-02-05 02:06:51 -0300

    Initial commit

---
+++ Pix2Pix_Project/trainer.py
@@ -0,0 +1,29 @@
+"""Pix2Pix implementation based on 
+https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
+Developed by David Dória https://github.com/daversd for
+2020-2021 B-pro Architectural Design RC4
+
+"""
+
+import torch
+import torch.onnx
+from torch.utils.tensorboard import SummaryWriter
+import pix2pix_helpers.util as util
+from pix2pix_helpers.create_dataset import ImageFolderLoader
+from pix2pix_helpers.pix2pix_model import Pix2PixModel
+from matplotlib import pyplot as plt
+import time
+import os
+
+##
+# High level setup
+##
+
+
+
+##
+# Main program
+##
+
+if __name__ == '__main__':
+    pass
---
+++ README.md
@@ -0,0 +1,2 @@
+# RC4_M3_C2_Base
+  Base solution for Class 03 of RC4's 2020-21 Technical Skills Module 3, Pix2Pix Implementation
